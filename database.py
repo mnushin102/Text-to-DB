9e9e5c521aa7a8782218fb4ad64fddf169b46dca
mnushin102 <100980092+mnushin102@users.noreply.github.com>
2023-12-07 14:29:37 -0800

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,24 @@
+import pymongo
 
+myclient = pymongo.MongoClient("mongodb+srv://merajnushin01:rSlzrlaRkmAVzat8@cluster0.lptxj1r.mongodb.net/?authMechanism=DEFAULT")
+mydb = myclient["Text-To-Database"]
+mycol = mydb["Database"]
+ 
+mydict1 = { "_id": "1", "first name": "Meraj", "last name": "Nushin" }
+mydict2 = { "_id": "2", "first name": "Daniel", "last name": "Moreno" }
+mydict3 = { "_id": "3", "first name": "Zoe", "last name": "White" }
+mydict4 = { "_id": "4", "first name": "Maitham", "last name": "Harb" }
+
+'''
+# Insert each attribute to the database collection 
+x = mycol.insert_one(mydict1)
+x = mycol.insert_one(mydict2)
+x = mycol.insert_one(mydict3)
+x = mycol.insert_one(mydict4)
+'''
+
+# Retrieved each added attribute from the database
+print(mycol.find_one())
+print(mycol.find_one({"_id" : "2"}))
+print(mycol.find_one({"_id" : "3"}))
+print(mycol.find_one({"_id" : "4"}))
