57505269f69f8c74b5df365faa39bcd1b9d4f9c6
mnushin102 <meraj_nushin@yahoo.com>
2024-02-14 16:49:08 -0800

    database python

--- py/database.py
+++ py/database.py
@@ -1,6 +1,6 @@
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb+srv://merajnushin01:LeezKhnkmnTbF9LT@cluster0.lptxj1r.mongodb.net/?authMechanism=DEFAULT")
+myclient = pymongo.MongoClient("mongodb+srv://merajnushin01:2ucgHvXV722ZUiwa@cluster0.lptxj1r.mongodb.net/")
 mydb = myclient["Text-To-Database"]
 mycol = mydb["Database"]
  
@@ -9,16 +9,16 @@ mydict2 = { "_id": "2", "first name": "Daniel", "last name": "Moreno" }
 mydict3 = { "_id": "3", "first name": "Zoe", "last name": "White" }
 mydict4 = { "_id": "4", "first name": "Maitham", "last name": "Harb" }
 
-'''
+
 # Insert each attribute to the database collection 
 x = mycol.insert_one(mydict1)
 x = mycol.insert_one(mydict2)
 x = mycol.insert_one(mydict3)
 x = mycol.insert_one(mydict4)
-'''
+
 
 # Retrieved each added attribute from the database
-print(mycol.find_one())
+print(mycol.find_one({"_id" : "1"}))
 print(mycol.find_one({"_id" : "2"}))
 print(mycol.find_one({"_id" : "3"}))
-print(mycol.find_one({"_id" : "4"}))
+print(mycol.find_one({"_id" : "4"})) 
