d0e1c0c7148c74c6f7555ed7103747b2ca380b2f
Maitham Harb <90878137+maithamharb@users.noreply.github.com>
2024-02-17 11:11:17 -0800

    Update dbConnect.js
    Removed create account functions and variables from this file

--- js/dbConnect.js
+++ js/dbConnect.js
@@ -1,13 +1,6 @@
 // Used: https://www.youtube.com/watch?v=-PdjUx9JZ2E for reference
 const mongoose = require('mongoose')
 
-const user = new mongoose.Schema({
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-});
-
-const Database = mongoose.model('Database', user);
-
 const dbConnection = async () => {
     try {
        await mongoose.connect("mongodb+srv://merajnushin01:2ucgHvXV722ZUiwa@cluster0.lptxj1r.mongodb.net/", {
@@ -20,15 +13,4 @@ const dbConnection = async () => {
     }
 }
 
-const createAccount = async (email, password) => {
-    try {
-        const new_user = new Database({ email, password });
-        await new_user.save();
-        console.log("The account was successfully created!");
-    } catch (error) {
-        console.error(error);
-    }
-};
-        
-
-module.exports = { dbConnection, createAccount };
+module.exports = dbConnection;
