43caeaf11414c79388fe4bef86643fc5a7081d45
Maitham Harb <90878137+maithamharb@users.noreply.github.com>
2024-02-17 00:07:50 -0800

    Update dbConnect.js
    Added a message when connection to MongoDB is successful, and also added the functions and variables needed to make the create account button work, and creates one while displaying an error if there ones in the account creation

--- js/dbConnect.js
+++ js/dbConnect.js
@@ -1,14 +1,34 @@
 // Used: https://www.youtube.com/watch?v=-PdjUx9JZ2E for reference
 const mongoose = require('mongoose')
 
+const user = new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+const Database = mongoose.model('Database', user);
+
 const dbConnection = async () => {
     try {
        await mongoose.connect("mongodb+srv://merajnushin01:2ucgHvXV722ZUiwa@cluster0.lptxj1r.mongodb.net/", {
         useUnifiedTopology: true,
         useNewUrlParser: true
        }); 
+       console.log("Connection to MongoDB was successful!");
     } catch (error) {
-        console.error(err);
+        console.error(error);
     }
 }
-module.exports = dbConnection;
+
+const createAccount = async (email, password) => {
+    try {
+        const new_user = new Database({ email, password });
+        await new_user.save();
+        console.log("The account was successfully created!");
+    } catch (error) {
+        console.error(error);
+    }
+};
+        
+
+module.exports = { dbConnection, createAccount };
