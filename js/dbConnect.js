468dfe73d34cb9d00425f054a84be24f6d1d6136
DannyM9021 <dmoreno.1109@gmail.com>
2024-02-16 21:26:23 -0800

    Created a module that connects to the database using js that can be used for database manipulation for other functions we will be implementing

---
+++ js/dbConnect.js
@@ -0,0 +1,14 @@
+// Used: https://www.youtube.com/watch?v=-PdjUx9JZ2E for reference
+const mongoose = require('mongoose')
+
+const dbConnection = async () => {
+    try {
+       await mongoose.connect("mongodb+srv://merajnushin01:2ucgHvXV722ZUiwa@cluster0.lptxj1r.mongodb.net/", {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+       }); 
+    } catch (error) {
+        console.error(err);
+    }
+}
+module.exports = dbConnection;
