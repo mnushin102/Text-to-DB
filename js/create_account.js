c45a1084561235b613306ae4be9f80340f44dda5
Maitham Harb <90878137+maithamharb@users.noreply.github.com>
2024-02-17 11:08:31 -0800

    Update create_account.js
    The code I put in dbConnect.js that was for creating the account, and move them here to a separate file

--- js/create_account.js
+++ js/create_account.js
@@ -1,1 +1,20 @@
+const mongoose = require('mongoose')
 
+const user = new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+const createAccount = async (email, password) => {
+    try {
+        const new_user = new Database({ email, password });
+        await new_user.save();
+        console.log("The account was successfully created!");
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+};
+
+module.exports = createAccount;
