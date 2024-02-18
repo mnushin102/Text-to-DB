dca6011ed6c76be098e5e2024282b23f0dccdc3d
mnushin102 <meraj_nushin@yahoo.com>
2024-02-17 19:08:26 -0800

    M

--- js/htmlmongo.js
+++ js/htmlmongo.js
@@ -1,3 +1,13 @@
 import "./dbConnect"
 
 require('mongodb').MongoClient; 
+
+const url = "mongodb+srv://merajnushin01:2ucgHvXV722ZUiwa@cluster0.lptxj1r.mongodb.net/"
+
+MongoClient.connect(url, function(err, dbConnect)
+{
+    if (err)
+        throw err; 
+    console.log("MongoDB database connected"); 
+    dbConnect.close(); 
+})
