3a8b095c5e50808e1d72b0bc58d94a198bab4904
Maitham Harb <90878137+maithamharb@users.noreply.github.com>
2024-02-23 15:26:00 -0800

    Update create_database.js
    Added the createNewDatabase() function in this file

--- js/create_database.js
+++ js/create_database.js
@@ -1,1 +1,21 @@
+const schema = {
+	// This is where the JSON schema should be
+	};
 
+const databaseSchema = new mongoose.Schema(schema);
+
+const Database = mongoose.model("Database", databaseSchema)
+
+async function createNewDatabase() {
+		try
+		{
+			const newDatabase = new Database({
+				tables: []
+			});
+
+			await newDatabase.save();
+			console.log("The new database was created successfully!");
+		} catch (error) {
+			console.error("There was an error in creating the new database:", error);
+		}
+}
