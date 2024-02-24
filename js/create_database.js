d3db879223356758a47be294886be9efdd2b67c9
Maitham Harb <90878137+maithamharb@users.noreply.github.com>
2024-02-24 15:55:22 -0800

    Update create_database.js
    Database with a functionality that would allow the user to create a database, which is just an example for now

--- js/create_database.js
+++ js/create_database.js
@@ -1,10 +1,27 @@
 const db = require("./databaseApp");
+const mongoose = require("mongoose");
 
 async function createNewDatabase() {
 	try
 	{
+		const databaseSchema = new mongoose.Schema({
+			name: { type: String, required: true },
+			description: { type: String }
+		});
+
+		const Database = mongoose.model("Database", databaseSchema);
+
+		const newdb = new Database({
+			name: "DatabaseName",
+			description: "DatabaseDescription"
+		});
+
+		await newDatabase.save();
+		
 		console.log("The new database was created successfully!");
 	} catch (error) {
 		console.error("There was an error in creating the new database:", error);
 	}
 }
+
+module.exports = createNewDatabase;
