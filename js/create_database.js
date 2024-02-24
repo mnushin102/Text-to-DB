24f8b65b20df349272c6a0b2cd956d912a8745af
Maitham Harb <90878137+maithamharb@users.noreply.github.com>
2024-02-24 14:03:57 -0800

    Update create_database.js
    Removed stuff that weren't necessary and connected this file to databaseApp.js

--- js/create_database.js
+++ js/create_database.js
@@ -1,21 +1,12 @@
-const schema = {
-	// This is where the JSON schema should be
-	};
-
-const databaseSchema = new mongoose.Schema(schema);
+const db = require("./databaseApp");
 
 const Database = mongoose.model("Database", databaseSchema)
 
 async function createNewDatabase() {
-		try
-		{
-			const newDatabase = new Database({
-				tables: []
-			});
-
-			await newDatabase.save();
-			console.log("The new database was created successfully!");
-		} catch (error) {
-			console.error("There was an error in creating the new database:", error);
-		}
+	try
+	{
+		console.log("The new database was created successfully!");
+	} catch (error) {
+		console.error("There was an error in creating the new database:", error);
+	}
 }
