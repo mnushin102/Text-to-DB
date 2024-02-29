3598d96df86d40381500445c83e346bab8e29fb9
Maitham Harb <90878137+maithamharb@users.noreply.github.com>
2024-02-28 19:38:00 -0800

    Update create_database.js
    Based on where we should generate the databases, I created a function called generateSQLFile() with a parameter called "attributes", which takes the attributes created by the user to generate the database in a SQL file, which the user can use for their own

--- js/create_database.js
+++ js/create_database.js
@@ -1,22 +1,35 @@
 const db = require("./databaseApp");
 const mongoose = require("mongoose");
+const fs = require("fs");
 
-async function createNewDatabase() {
+function generateSQLFile(attributes) {
+	let sqlContent = "Create table if it doesn't exist (The table name) (\n";
+
+	attributes.forEach((attribute, i) => {
+		sqlContent += "\t${attribute.name} ${attribute.type}";
+
+		if (i < attributes.length - 1) {
+			sqlContent += ",\n";
+		} else {
+			sqlContent += "\n";
+		}
+	});
+
+	sqlContent += ");\n";
+	
+	fs.writeFile("generated_database.sql", sqlContent, (err) => {
+		if (err) {
+			console.error("There is an error writing the SQL file:", err);
+		} else {
+			console.log("The sql file was generated successfully!");
+		}
+	});
+}
+
+async function createNewDatabase(attributes) {
 	try
 	{
-		const databaseSchema = new mongoose.Schema({
-			name: { type: String, required: true },
-			description: { type: String }
-		});
-
-		const Database = mongoose.model("Database", databaseSchema);
-
-		const newdb = new Database({
-			name: "DatabaseName",
-			description: "DatabaseDescription"
-		});
-
-		await newDatabase.save();
+		generateSQLFile(attributes);
 		
 		console.log("The new database was created successfully!");
 	} catch (error) {
