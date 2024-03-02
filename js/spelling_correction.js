5780b03bfff24277706b7edb4f83508c31f0b01d
Maitham Harb <90878137+maithamharb@users.noreply.github.com>
2024-03-01 21:32:24 -0800

    Update spelling_correction.js
    Using the Node.js "natural" library, I created a function called "spelling_correction()" with parameter "text" that splits the inputted text into words, and goes through each one, finds a suggestion, if it finds one, it returns the correct word, if it doesn't, it returns the same word, then after all that match all those words together to form the correct spelling

--- js/spelling_correction.js
+++ js/spelling_correction.js
@@ -1,3 +1,25 @@
-function spelling_correction() {
+const natural = require("natural);
 
+const textSpelling = new natural.Spellcheck();
+
+function spelling_correction(text) {
+    const words = text.split(/\s+/);
+
+    const correctedText = words.map(word => {
+        if(!textSpelling.isCorrect(word)) {
+            const suggestion = textSpelling.getCorrections(word, 1);
+
+            if(suggestion.length > 0) {
+                return suggestion[0];
+            } else {
+                return word
+            }
+        } else {
+            return word;
+        }
+    });
+
+    return correctedText.join(' ');
 }
+
+module.exports = spelling_correction;
