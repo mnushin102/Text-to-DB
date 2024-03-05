59ce6609a9aeaa71e7e4c8b9a0d49bb2f6c7de24
Maitham Harb <90878137+maithamharb@users.noreply.github.com>
2024-03-04 17:35:16 -0800

    Update spelling_correction.js
    The function now corrects the inputted words, I put comments on different lines to explain what I did in each one
    
    I used the file "word_frequency.txt" to search through all its words to correct the spelling
    
    Used both the "natural" for correcting, and "fs" for read the txt file, make sure to install both of them

--- js/spelling_correction.js
+++ js/spelling_correction.js
@@ -1,25 +1,54 @@
 const natural = require("natural");
+const fs = require("fs");
 
-const textSpelling = new natural.Spellcheck();
+// Load the word_frequency.txt file
+const dictionaryPath = "word_frequency.txt";
+const dictionaryContent = fs.readFileSync(dictionaryPath, "utf-8");
 
+// Pre-process the dictionary content into a set for better and faster lookups
+const dictionarySet = new Set(dictionaryContent.split("\n").map(line => line.split(' ')[0].toLowerCase()));
+
+// This is the function that will perform the spelling correction
 function spelling_correction(text) {
-    const words = text.split(/\s+/);
+    // Tokenize the input text into words and punctuations
+    const tokens = text.match(/\b\w+\b|\s/g);
 
-    const correctedText = words.map(word => {
-        if(!textSpelling.isCorrect(word)) {
-            const suggestion = textSpelling.getCorrections(word, 1);
+    // Correct each word
+    let correctedText = "";
 
-            if(suggestion.length > 0) {
-                return suggestion[0];
+    for (let i = 0; i < tokens.length; i++) {
+        const token = tokens[i];
+
+        // If what is given is a word
+        if (/\w+/.test(token)) {
+            const word = token.toLowerCase();
+
+            if (dictionarySet.has(word)) {
+                // If the word is in the dictionary, keep the word as it is and add it
+                correctedText += token;
             } else {
-                return word
+                // Check if the word has consecutive identical characters, if it has remove these extra characters
+                const correctedWord = word.replace(/(.)\1+/g, "$1");
+                
+                // Using the Jaro-Winkler distance, it will give suggestions based on the word
+                const suggestions = Array.from(dictionarySet)
+                    .filter(dictWord => Math.abs(dictWord.length - correctedWord.length) <= 2)
+                    .map(dictWord => ({ word: dictWord, distance: natural.JaroWinklerDistance(correctedWord, dictWord) }))
+                    .filter(({ distance }) => distance > 0.7)
+                    .sort((a, b) => b.distance - a.distance);
+
+                correctedText += suggestions.length > 0 ? suggestions[0].word : token;
             }
         } else {
-            return word;
+            // In case it is a punctuation or a whitespace, keep it also
+            correctedText += token;
         }
-    });
+    }
 
-    return correctedText.join(' ');
+    // Capitalize the first letter of the corrected text
+    correctedText = correctedText.charAt(0).toUpperCase() + correctedText.slice(1);
+
+    return correctedText;
 }
 
 module.exports = spelling_correction;
