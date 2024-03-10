26ca8a628092a0b99240ceaac9a783d43131d491
Maitham Harb <90878137+maithamharb@users.noreply.github.com>
2024-03-09 22:41:19 -0800

    Update test.js
    Fixed a small thing

--- js/test.js
+++ js/test.js
@@ -3,18 +3,20 @@ const spelling_correction = require("../js/spelling_correction.js");
 
 // Sample text to test
 const sampleTexts = [
-    "Thiis is somee samplee texxt to testt the spelling correctioon function",
-    "I hvee aa llot of worrkkk to doo todaay",
-    "Ie wass thinkinnngg aboouutt goinngg tto the mooviie tonightt",
-    "Wheeree aree youu goiingg to thiss yeearr"
+     "Thiis is somee samplee texxt to testt the spelling correctioon function",
+     "I hvee aa llot of worrkkk to doo todaay",
+     "Ie wass thinkinnngg aboouutt goinngg tto the mooviie tonightt",
+     "Wheeree aree youu goiingg to thiss yeearr"
 ];
 
 console.log("\n");
 
-// Correct the sample texts
+// Correct the sample texts and log them with dotted lines between each pair
 sampleTexts.forEach(text => {
     const correctedText = spelling_correction(text);
     console.log("Input:", text);
     console.log("Output:", correctedText);
-    console.log(); // Add a blank line between each pair of input and output
+    console.log("-".repeat(50));
 });
+
+console.log("\n");
