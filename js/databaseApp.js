170d43f8cdd25d16e2886bb573ee557308f75b1d
mnushin102 <meraj_nushin@yahoo.com>
2024-03-11 10:08:22 -0700

    Fixed the layout of the project page

--- js/databaseApp.js
+++ js/databaseApp.js
@@ -1,4 +1,4 @@
-import "../models/dataSchema.js"
+
 
 // Import the dependencies for backend
 const bodyParser = require("body-parser"); 
@@ -8,20 +8,26 @@ const app = express();
 const mongoose = require('mongoose');
 const insertRoute = require("../routes/databaseApp"); 
 
-app.use(express.json()); 
 app.use(bodyParser.urlencoded({extended: true})); 
 
 // Set up a connection to MongoDB 
 mongoose.connect("mongodb+srv://merajnushin01:2ucgHvXV722ZUiwa@cluster0.lptxj1r.mongodb.net/user_info", {useNewUrlParser: true}, {useUnifiedTopology: true}); 
 
+const dataSchema = {
+    data_variable_1: String, 
+    data_variable_2: String 
+}
+
+const Data = mongoose.model("Data", dataSchema); 
+
 // This function lets the user input the following data to upload to Mongo
-app.get("/data", function(req, res) {
-    res.sendFile(__dirname + "../html/project.html"); 
+app.get("/html/project.html", function(req, res) {
+    res.sendFile(__dirname + "project.html"); 
 }); 
 
-    // Manually let the user insert the following fields
-    // so that the user can post these in the database 
-app.post("/data", function(req, res) {
+// Manually let the user insert the following fields
+// so that the user can post these in the database 
+app.post("/html/project.html", function(req, res) {
     // Assign the schema within these fields
     let addData = new Data({
         data_variable_1: req.body.data_variable_1,
@@ -38,11 +44,11 @@ app.post("/data", function(req, res) {
             console.log("ALL GOOD!")
         }
     }); 
-    res.redirect("/data"); 
+    res.redirect("/html/project.html"); 
 }); 
 
 // Make sure the server is listening to 3001 so that we know we're connected to the MongoDB database
-app.listen(3001, function() {
+app.listen(3000, function() {
     try {
         console.log("Successfully connected to MongoDB"); 
     }
