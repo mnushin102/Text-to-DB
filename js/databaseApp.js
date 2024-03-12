3c563c9d9cf19a49d756c148f19f5509ec33fde6
mnushin102 <meraj_nushin@yahoo.com>
2024-03-11 23:12:39 -0700

    ;)

--- js/databaseApp.js
+++ js/databaseApp.js
@@ -6,6 +6,7 @@ const databaseSchema = require("../models/dataSchema");
 const express = require('express'); 
 const app = express();
 const mongoose = require('mongoose');
+const schema = mongoose.Schema; 
 const insertRoute = require("../routes/databaseApp"); 
 
 app.use(bodyParser.urlencoded({extended: true})); 
@@ -13,42 +14,37 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Set up a connection to MongoDB 
 mongoose.connect("mongodb+srv://merajnushin01:2ucgHvXV722ZUiwa@cluster0.lptxj1r.mongodb.net/user_info", {useNewUrlParser: true}, {useUnifiedTopology: true}); 
 
-const dataSchema = {
+const dataSchema = new schema({
     data_variable_1: String, 
     data_variable_2: String 
-}
+})
 
 const Data = mongoose.model("Data", dataSchema); 
 
 // This function lets the user input the following data to upload to Mongo
-app.get("/html/project.html", function(req, res) {
-    res.sendFile(__dirname + "project.html"); 
+app.get("/home", function(req, res) {
+    res.render("project"); 
 }); 
 
 // Manually let the user insert the following fields
 // so that the user can post these in the database 
-app.post("/html/project.html", function(req, res) {
+app.post("/", function(req, res) {
     // Assign the schema within these fields
-    let addData = new Data({
+    const addData = new Data({
         data_variable_1: req.body.data_variable_1,
         data_variable_2: req.body.data_variable_2 
     }); 
 
     // save the inputted fields to the database 
-    addData.save(function(err){
-        if (err){
-            console.log("ERROR!")
-        }
-
-        else{
-            console.log("ALL GOOD!")
-        }
+    addData.save(function(err, addData){
+        if (err) throw error
+        res.json(addData); 
     }); 
-    res.redirect("/html/project.html"); 
+    res.redirect("/"); 
 }); 
 
 // Make sure the server is listening to 3001 so that we know we're connected to the MongoDB database
-app.listen(3000, function() {
+app.listen(3001, function() {
     try {
         console.log("Successfully connected to MongoDB"); 
     }
