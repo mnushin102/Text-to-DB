95fb5aeff6d07eff20112ddb7ea69d071f7622f8
Maitham Harb <90878137+maithamharb@users.noreply.github.com>
2024-03-28 01:47:11 -0700

    Update Database.js
    Removed the function "generateSQLForClass()" from here because I moved it to the file "create_database.js" and made the necessary call
    
    Just to make this file look better

--- routes/Database.js
+++ routes/Database.js
@@ -4,7 +4,8 @@
 const express = require("express");
 const router = express.Router();
 const Database = require("../models/Database");
-const fs = require('fs');
+const fs = require("fs");
+const generateSQLForClass = require("../js/create_database.js");
 
 // Statuses defined by https://expressjs.com/en/guide/error-handling.html
 const ERROR_400 = 400; // User inputs incorrect data
@@ -12,49 +13,6 @@ const ERROR_404 = 404; // Not found error
 const ERROR_500 = 500; // Server side error
 const SUCCESS_200 = 200; // Request was successful
 const SUCCESS_201 = 201; // Successfully created (an object)
-
-// Function to generate SQL for a class schema
-async function generateSQLForClass(className) {
-    try {
-        // Find the document with the given class name
-        const databaseEntry = await Database.findOne({ "database_elements.class_name": className });
-        
-        if (!databaseEntry) {
-            return "Class was not found in the database.";
-        }
-
-        // Find the class object within the database entry
-        const classObject = databaseEntry.database_elements.find(element => element.class_name === className);
-
-        if (!classObject) {
-            return "Class was not found in the database.";
-        }
-
-        // Extract attribute list
-        const attributes = classObject.attribute_list;
-
-        // Generate SQL CREATE TABLE statement
-        let sql = `CREATE TABLE ${className} (\n`;
-
-        attributes.forEach(attribute => {
-            sql += `    ${attribute.attribute} ${attribute.type},\n`;
-        });
-
-        // Remove trailing comma and newline
-        sql = sql.slice(0, -2) + '\n';
-
-        // Closing bracket
-        sql += ');';
-
-        // Write SQL to file
-        fs.writeFileSync('generated_file.sql', sql);
-
-        return "The SQL file was successfully generated.";
-    } catch (error) {
-        console.error("Error generating SQL:", error);
-        return "An error occurred while generating the SQL file.";
-    }
-}
 
 // Retrieves database projects based on the user
 router.get('/database', async (req,res) => {
