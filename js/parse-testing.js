8a8a549ca366105eac47c5d13e8b3b79a2189239
Ethan <ethancesinowhite@gmail.com>
2024-04-06 13:55:55 -0700

    Got the basic parsing for to and from SQL working and commented. Will be updated to teams needs and potentially more cases.

--- js/parse-testing.js
+++ js/parse-testing.js
@@ -3,3 +3,7 @@ const test = require("./parse")
 console.log("Test from SQL\n")
 var text = "CREATE TABLE emp\nempno INT PRIMARY KEY\nename VARCHAR(10)\njob VARCHAR(9)\nmgr INT NULL"
 console.log(test(text, false))
+
+console.log("\nTest to SQL\n")
+var text = "Class emp{\nint empno\nstring ename\nstring job\nint mgr}"
+console.log(test(text, true))
--- js/parse.js
+++ js/parse.js
@@ -7,13 +7,72 @@ function parse(text = "", toSQL = True){
     var output = ""
     //parses the text into sql
     if(toSQL == true){
-        
+        //divides the input based on lines into array
+        var input = text.split('\n')
+        //used to loop through the lines
+        var inputLength = input.length
+        var input2
+        //is and did are just to check if it is a Class so it wont reverse it, and add a "("
+        var isClass = false
+        var didClass = false
+        //loops through each line of the text
+        for(var i = 0; i < inputLength; i++){
+            input2 = input[i].split(" ")
+            var isClass = false      
+            var input2Length = input2.length
+            //checks if the current line is a "Class" line
+            for(var j = 0; j < input2Length; j++){
+                str = input2[j].toLowerCase()
+                if(str.includes("class")){
+                    isClass = true
+                }
+            }
+            //if it isnt a class line it reverses the words so the variable name is after
+            if(isClass == false){
+                input2.reverse()
+            } else {
+                isClass = false
+            }
+            for(var j = 0; j < input2Length; j++){
+                //converts the current element to lower case
+                str = input2[j].toLowerCase()
+                if(str.includes('{') || str.includes('}')){
+                    str = str.substring(0,str.length-1)
+                }
+                if(str.includes("class")){
+                    output = output + "CREATE TABLE "
+                    isClass = true
+                }
+                //equivalent of "int"
+                else if(str.includes("int")){
+                    output = output + " INT"
+                }
+                //equivalent of "string"
+                else if(str.includes("string")){
+                    output = output + " VARCHAR(10)"
+                } 
+                //equivalent of a variable name
+                else {
+                    output = output + str
+                }
+            }
+            //if this line was a class adds '{' to the end
+            if(isClass == true){
+                output = output + '('
+                isClass = false
+                didClass = true
+            }
+            output = output + '\n'
+        }  
+        //adds ')' to the end of an entire class
+        if(didClass == true){
+            output = output + ')'
+        }
+        return output
     //parses the text from sql
     } else {
         //divides the input based on lines into array
         var input = text.split('\n');
-        /*console.log(input)
-        console.log("Input length: " + input.length)*/
         //used to loop through the lines
         var inputLength = input.length
         var input2
@@ -23,8 +82,6 @@ function parse(text = "", toSQL = True){
         //loops through each line of the text
         for(var i = 0; i < inputLength; i++){
             input2 = input[i].split(" ")
-            /*console.log(input2)
-            console.log("Input2 length: " + input2.length)*/
             var isClass = false      
             var input2Length = input2.length
             //checks if the current line is a "Class" line
